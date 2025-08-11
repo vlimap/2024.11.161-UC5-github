4e2f7f3fbcb3f01ac172010d30294e73ab7524c2
MARLON LIMA DE MELO <a92285985@rn.senac.local>
2025-08-11 20:06:47 -0300

    refactor(routes): comment out unused route imports and middleware

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ app.use(express.json());
 
 // Rotas principais (exemplo)
 // Rotas dos módulos
-import agendaRoutes from "./src/modules/agenda/routes.js";
-import clientesRoutes from "./src/modules/clientes/routes.js";
-import colaboradoresRoutes from "./src/modules/colaboradores/routes.js";
-import servicosRoutes from "./src/modules/servicos/routes.js";
-import produtosRoutes from "./src/modules/produtos/routes.js";
-import pagamentosRoutes from "./src/modules/pagamentos/routes.js";
-import estoqueRoutes from "./src/modules/estoque/routes.js";
-import relatoriosRoutes from "./src/modules/relatorios/routes.js";
-import autenticacaoRoutes from "./src/modules/autenticacao/routes.js";
-import vendasRoutes from "./src/modules/vendas/routes.js";
+// import agendaRoutes from './src/modules/agenda/routes.js';
+// import clientesRoutes from './src/modules/clientes/routes.js';
+// import colaboradoresRoutes from './src/modules/colaboradores/routes.js';
+// import servicosRoutes from './src/modules/servicos/routes.js';
+// import produtosRoutes from './src/modules/produtos/routes.js';
+// import vendasRoutes from './src/modules/vendas/routes.js';
+// import pagamentosRoutes from './src/modules/pagamentos/routes.js';
+// import estoqueRoutes from './src/modules/estoque/routes.js';
+import relatoriosRoutes from "./src/modules/relatorios/routes/relatorio.route.js";
+// import autenticacaoRoutes from './src/modules/autenticacao/routes.js';
 
-app.use("/agenda", agendaRoutes);
-app.use("/clientes", clientesRoutes);
-app.use("/colaboradores", colaboradoresRoutes);
-app.use("/servicos", servicosRoutes);
-app.use("/produtos", produtosRoutes);
-app.use("/vendas", vendasRoutes);
-app.use("/pagamentos", pagamentosRoutes);
-app.use("/estoque", estoqueRoutes);
-app.use("/relatorios", relatoriosRoutes);
-app.use("/autenticacao", autenticacaoRoutes);
+// app.use('/agenda', agendaRoutes);
+// app.use('/clientes', clientesRoutes);
+// app.use('/colaboradores', colaboradoresRoutes);
+// app.use('/servicos', servicosRoutes);
+// app.use('/produtos', produtosRoutes);
+// app.use('/vendas', vendasRoutes);
+// app.use('/pagamentos', pagamentosRoutes);
+// app.use('/estoque', estoqueRoutes);
+app.use(relatoriosRoutes);
+// app.use('/autenticacao', autenticacaoRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
